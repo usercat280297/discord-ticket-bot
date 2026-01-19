9e2bf15a6ef113f06b46465cdab483a01f70930a
usercat280297 <thanh1084@epu.edu.vn>
2026-01-19 16:06:55 +0700

    Add audioop workaround for Python 3.13+ compatibility

---
+++ audioop.py
@@ -0,0 +1,74 @@
+# Dummy audioop module for Python 3.13+
+# This is a workaround for systems where audioop was removed
+
+def add(*args, **kwargs):
+    raise NotImplementedError("audioop is not available in this environment")
+
+def adpcm2lin(*args, **kwargs):
+    raise NotImplementedError("audioop is not available in this environment")
+
+def alaw2lin(*args, **kwargs):
+    raise NotImplementedError("audioop is not available in this environment")
+
+def avg(*args, **kwargs):
+    raise NotImplementedError("audioop is not available in this environment")
+
+def avgpp(*args, **kwargs):
+    raise NotImplementedError("audioop is not available in this environment")
+
+def bias(*args, **kwargs):
+    raise NotImplementedError("audioop is not available in this environment")
+
+def cross(*args, **kwargs):
+    raise NotImplementedError("audioop is not available in this environment")
+
+def finalize(*args, **kwargs):
+    raise NotImplementedError("audioop is not available in this environment")
+
+def findfit(*args, **kwargs):
+    raise NotImplementedError("audioop is not available in this environment")
+
+def findmax(*args, **kwargs):
+    raise NotImplementedError("audioop is not available in this environment")
+
+def getsample(*args, **kwargs):
+    raise NotImplementedError("audioop is not available in this environment")
+
+def lin2adpcm(*args, **kwargs):
+    raise NotImplementedError("audioop is not available in this environment")
+
+def lin2alaw(*args, **kwargs):
+    raise NotImplementedError("audioop is not available in this environment")
+
+def lin2ulaw(*args, **kwargs):
+    raise NotImplementedError("audioop is not available in this environment")
+
+def max(*args, **kwargs):
+    raise NotImplementedError("audioop is not available in this environment")
+
+def minmax(*args, **kwargs):
+    raise NotImplementedError("audioop is not available in this environment")
+
+def mul(*args, **kwargs):
+    raise NotImplementedError("audioop is not available in this environment")
+
+def ratecv(*args, **kwargs):
+    raise NotImplementedError("audioop is not available in this environment")
+
+def reverse(*args, **kwargs):
+    raise NotImplementedError("audioop is not available in this environment")
+
+def rms(*args, **kwargs):
+    raise NotImplementedError("audioop is not available in this environment")
+
+def tomono(*args, **kwargs):
+    raise NotImplementedError("audioop is not available in this environment")
+
+def tostereo(*args, **kwargs):
+    raise NotImplementedError("audioop is not available in this environment")
+
+def ulaw2lin(*args, **kwargs):
+    raise NotImplementedError("audioop is not available in this environment")
+
+# Module-level constants
+error = Exception
